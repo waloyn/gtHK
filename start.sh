fbccd66dd6007ba9b406c4bcd75945566ae37529
PeterRain <waloyn@gmail.com>
2022-03-09 09:21:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,9 @@ TMP="$(mktemp -d)"
 #Get Gost 
 curl --retry 5 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL ${DLURL} -o ${TMP}/gost.gz
 busybox gzip -d ${TMP}/gost.gz
-chmod 755 ${TMP}/gost
 
 #Install Gost
 install -m 755 ${TMP}/gost ${RUNTIME}
 
 #Run Gost
-${RUNTIME}/gost ${CMD}
+${RUNTIME}/gost ${CMD}:${PORT}${MDATA}
